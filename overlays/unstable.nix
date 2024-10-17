1bb7147f1e046775f4fd8011bf093e6e60ed274d
Dom Rodriguez <shymega@shymega.org.uk>
2024-10-17 14:41:31 +0100

    chore: Sync all repos
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- overlays/unstable.nix
+++ overlays/unstable.nix
@@ -10,11 +10,9 @@ let
   importUnstableOverlay =
     overlay: lib.composeExtensions (_: _: { __inputs = inputs; }) (import (./unstable + "/${overlay}"));
 
-  unstableOverlays = lib.mapAttrs'
-    (
-      overlay: _: lib.nameValuePair (lib.removeSuffix ".nix" overlay) (importUnstableOverlay overlay)
-    )
-    (builtins.readDir ./unstable);
+  unstableOverlays = lib.mapAttrs' (
+    overlay: _: lib.nameValuePair (lib.removeSuffix ".nix" overlay) (importUnstableOverlay overlay)
+  ) (builtins.readDir ./unstable);
 in
 {
   unstable = import inputs.nixpkgs-unstable {
