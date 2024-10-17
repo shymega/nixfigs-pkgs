f650926ad0ee2dc73fd2de95d079ad5c46114900
Dom Rodriguez <shymega@shymega.org.uk>
2024-10-17 14:41:31 +0100

    chore: Sync all repos
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- overlays/stable/evdi.nix
+++ overlays/stable/evdi.nix
@@ -2,16 +2,37 @@
 #
 # SPDX-License-Identifier: GPL-3.0-only
 
-_: prev: rec {
-  evdi = prev.linuxPackages_xanmod_latest.evdi.overrideAttrs (_final: _prev: rec {
-    version = "1.14.7";
-    src = prev.fetchFromGitHub {
-      owner = "DisplayLink";
-      repo = "evdi";
-      rev = "refs/tags/v${version}";
-      hash = "sha256-z3GawjaokbmmUC1LihwGSnF3tUp9n/FO+kDiWvBq+mY=";
-    };
-  });
+_: prev:
+let
+  inherit (prev) lib;
+  myCompatibleKernelPackages = lib.filterAttrs (
+    name: kernelPackages:
+    (lib.hasInfix "_xanmod" name)
+    && (builtins.tryEval kernelPackages).success
+    && (
+      (!kernelPackages.zfs.meta.broken)
+      && (!kernelPackages.nvidia_x11.meta.broken)
+      && (!kernelPackages.evdi.meta.broken)
+    )
+  ) prev.linuxKernel.packages;
+  latestKernelPackage = lib.last (
+    lib.sort (a: b: (lib.versionOlder a.kernel.version b.kernel.version)) (
+      builtins.attrValues myCompatibleKernelPackages
+    )
+  );
+in
+rec {
+  evdi = latestKernelPackage.evdi.overrideAttrs (
+    _final: _prev: rec {
+      version = "1.14.7";
+      src = prev.fetchFromGitHub {
+        owner = "DisplayLink";
+        repo = "evdi";
+        rev = "refs/tags/v${version}";
+        hash = "sha256-z3GawjaokbmmUC1LihwGSnF3tUp9n/FO+kDiWvBq+mY=";
+      };
+    }
+  );
 
   displaylink = prev.displaylink.override { inherit evdi; };
 
