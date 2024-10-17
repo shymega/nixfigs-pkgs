5ee418412c0dfd39da2833b1d385ab03808b5f0e
Dom Rodriguez <shymega@shymega.org.uk>
2024-10-17 14:41:31 +0100

    chore: Sync all repos
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- overlays/stable/evdi.nix
+++ overlays/stable/evdi.nix
@@ -3,15 +3,17 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 _: prev: rec {
-  evdi = prev.linuxPackages_xanmod_latest.evdi.overrideAttrs (_final: _prev: rec {
-    version = "1.14.7";
-    src = prev.fetchFromGitHub {
-      owner = "DisplayLink";
-      repo = "evdi";
-      rev = "refs/tags/v${version}";
-      hash = "sha256-z3GawjaokbmmUC1LihwGSnF3tUp9n/FO+kDiWvBq+mY=";
-    };
-  });
+  evdi = prev.linuxPackages_xanmod_latest.evdi.overrideAttrs (
+    _final: _prev: rec {
+      version = "1.14.7";
+      src = prev.fetchFromGitHub {
+        owner = "DisplayLink";
+        repo = "evdi";
+        rev = "refs/tags/v${version}";
+        hash = "sha256-z3GawjaokbmmUC1LihwGSnF3tUp9n/FO+kDiWvBq+mY=";
+      };
+    }
+  );
 
   displaylink = prev.displaylink.override { inherit evdi; };
 
