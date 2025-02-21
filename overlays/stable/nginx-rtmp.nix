64361d65f693e697f6915ed11cb168c83f7dfcb8
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:19:34 +0000

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- overlays/stable/nginx-rtmp.nix
+++ overlays/stable/nginx-rtmp.nix
@@ -1,13 +1,11 @@
 # SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
 #
 # SPDX-License-Identifier: GPL-3.0-only
-
 #
-
 _final: prev: {
   nginx-rtmp-patched = prev.nginxStable.override (oldAttrs: {
     pname = "nginx-rtmp-patched";
     version = "stable";
-    modules = oldAttrs.modules ++ [ prev.nginxModules.rtmp ];
+    modules = oldAttrs.modules ++ [prev.nginxModules.rtmp];
   });
 }
