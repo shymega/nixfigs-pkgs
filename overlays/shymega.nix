cd2e149585012c18f4e35f6d97646fee7081f801
Dom Rodriguez <shymega@shymega.org.uk>
2025-02-21 22:19:33 +0000

    feat: Initial commit
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

---
+++ overlays/shymega.nix
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+#
+
+{ inputs, lib, ... }:
+_final: prev:
+let
+  importShymegaOverlay =
+    overlay: lib.composeExtensions (_: _: { __inputs = inputs; }) (import (./shymega + "/${overlay}"));
+
+  shymegaOverlays = lib.mapAttrs'
+    (
+      overlay: _: lib.nameValuePair (lib.removeSuffix ".nix" overlay) (importShymegaOverlay overlay)
+    )
+    (builtins.readDir ./shymega);
+in
+{
+  shymega = import inputs.nixpkgs-shymega {
+    inherit (prev) system;
+    config = inputs.self.nixpkgs-config;
+    overlays = builtins.attrValues shymegaOverlays;
+  };
+}
---
+++ pkgs/totp/default.nix
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2024 Dom Rodriguez <shymega@shymega.org.uk
+#
+# SPDX-License-Identifier: GPL-3.0-only
+
+#
+
+{ python3Packages, fetchPypi }:
+python3Packages.buildPythonApplication rec {
+  pname = "totp";
+  version = "1.3.0";
+  pyproject = true;
+
+  src = fetchPypi {
+    pname = "totp";
+    inherit version;
+    hash = "sha256-reWv3pH1NaO7wKAt4kbfrewKS6RlpZFH4NsYb2ai43I=";
+  };
+
+  build-system = with python3Packages; [
+    setuptools
+    setuptools_scm
+  ];
+
+  dependencies = with python3Packages; [ onetimepass ];
+}
