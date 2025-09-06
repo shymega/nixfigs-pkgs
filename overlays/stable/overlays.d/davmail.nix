b0467d6e5ee12c695e6f035f2874754858027af5
Dom Rodriguez <shymega@shymega.org.uk>
2025-09-06 10:36:10 +0100

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- overlays/stable/overlays.d/davmail.nix
+++ overlays/stable/overlays.d/davmail.nix
@@ -4,12 +4,12 @@
 _: prev: {
   davmail = prev.davmail.overrideAttrs (oldAttrs: rec {
     inherit (oldAttrs) pname;
-    version = "6.3.0";
+    version = "6.4.0";
     rev = 3627;
 
     src = prev.fetchzip {
-      url = "https://ci.appveyor.com/api/projects/mguessan/davmail/artifacts/dist%2Fdavmail-6.3.0-trunk.zip?job=Environment%3A%20JAVA_HOME%3DC%3A%5CProgram%20Files%5CJava%5Cjdk1.8.0";
-      hash = "sha256-TJOdwoEdOehy3jxqwGs6FZLVzVZBkRRDFnggjeKOm2k=";
+      url = "https://ci.appveyor.com/api/projects/mguessan/davmail/artifacts/dist%2Fdavmail-6.4.0-trunk.zip?job=Environment%3A%20JAVA_HOME%3DC%3A%5CProgram%20Files%5CJava%5Cjdk1.8.0";
+      hash = "sha256-3HNiRH+TVTu8xAHdDwJ8LIOlHksmwbk8Ql5KVv1TFxA=";
       extension = "zip";
       stripRoot = false;
     };
