318b46fbe275254d5df09103c9396e5b00a15ec3
Dom Rodriguez <shymega@shymega.org.uk>
2025-09-07 00:02:42 +0100

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- overlays/unstable/overlays.d/goimapnotify.nix
+++ overlays/unstable/overlays.d/goimapnotify.nix
@@ -2,21 +2,14 @@
 #
 # SPDX-License-Identifier: GPL-3.0-only
 _: prev: {
-  goimapnotify = prev.goimapnotify.overrideAttrs
-    (oldAttrs: {
+  goimapnotify =
+    prev.goimapnotify.overrideAttrs
+    (_oldAttrs: {
       src = prev.fetchFromGitLab {
         owner = "shackra";
         repo = "goimapnotify";
         rev = "4ffe653018b4b295a1ca2634e4108455cb19000a";
         hash = "sha256-fbYujTAU5Vz8DsZQB8TAZYUja92WBK/U/fwgFHQ9TC4=";
       };
-      patches =
-        (oldAttrs.patches or [ ])
-        ++ [
-          (prev.fetchpatch {
-            url = "https://gist.githubusercontent.com/shymega/c6d5701c2d8be448638f59de3842ae33/raw/930d46071c91f2276938505e7d5bb5ea891df850/0001-fix-Disable-Running-commit-messsage.patch";
-            sha256 = "sha256-NSjCrjMDT2mpUSeDVFuboowFlMCE9KZYyMj0x/AlY1Y=";
-          })
-        ];
     });
 }
