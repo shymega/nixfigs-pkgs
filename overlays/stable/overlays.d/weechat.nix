0c2fbe90351275bfaa5d91cb013d9dab07f00a0b
Dom Rodriguez <shymega@shymega.org.uk>
2025-11-30 21:16:02 +0000

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- overlays/stable/overlays.d/weechat.nix
+++ overlays/stable/overlays.d/weechat.nix
@@ -5,31 +5,16 @@
 _: prev: {
   weechatWithMyPlugins = prev.weechat.override {
     configure = {availablePlugins, ...}: {
-      scripts = let
-        weechat-matrix = prev.weechatScripts.weechat-matrix.overrideAttrs (oldAttrs: {
-          patches =
-            oldAttrs.patches
-            or []
-            ++ [
-              (prev.fetchpatch {
-                url = "https://patch-diff.githubusercontent.com/raw/poljar/weechat-matrix/pull/349.diff?full_index=1";
-                hash = "sha256-oJ+vfdSWhEWdKG52c9kiOXCeOscq+sSL3N8CoI7OiZo=";
-              })
-            ];
-        });
-      in
-        with prev.weechatScripts;
-          [
-            autosort
-            edit
-            url_hint
-            wee-slack
-            weechat-autosort
-            weechat-go
-            weechat-notify-send
-            zncplayback
-          ]
-          ++ [weechat-matrix];
+      scripts = with prev.weechatScripts; [
+        autosort
+        edit
+        url_hint
+        wee-slack
+        weechat-autosort
+        weechat-go
+        weechat-notify-send
+        zncplayback
+      ];
       plugins = builtins.attrValues availablePlugins;
     };
   };
