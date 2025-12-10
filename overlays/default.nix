144e50f2763cafb5d0df1b336a30beaf5369c1bd
Dom Rodriguez <shymega@shymega.org.uk>
2025-12-10 20:06:52 +0000

    feat: Update to use alternative to Chaotic Nyx for kernels/ZFS
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- overlays/default.nix
+++ overlays/default.nix
@@ -21,11 +21,11 @@
     [
       agenix.overlays.default
       android-nixpkgs.overlays.default
-      chaotic.overlays.cache-friendly
       deckcheatz.overlays.default
       devenv.overlays.default
       dzr-taskwarrior-recur.overlays.default
       nix-alien.overlays.default
+      nix-cachyos-kernel.overlays.default
       nix-doom-emacs-unstraightened.overlays.default
       nur.overlays.default
       shypkgs-public.overlays.default
