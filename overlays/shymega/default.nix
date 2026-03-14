afc9a7d405d9767c67b358513cd2503a8f989bb3
Dom Rodriguez <shymega@shymega.org.uk>
2026-03-14 01:05:30 +0000

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- overlays/shymega/default.nix
+++ overlays/shymega/default.nix
@@ -2,20 +2,10 @@
 #
 # SPDX-License-Identifier: GPL-3.0-only
 #
-{
-  inputs,
-  lib,
-  ...
-}: final: _prev: let
-  importShymegaOverlay = overlay: lib.composeExtensions (_: _: {__inputs = inputs;}) (import (./enabled.d + "/${overlay}"));
-
-  shymegaOverlays = lib.mapAttrs' (
-    overlay: _: lib.nameValuePair (lib.removeSuffix ".nix" overlay) (importShymegaOverlay overlay)
-  ) (builtins.readDir ./enabled.d);
-in {
+{inputs, ...}: final: _prev: {
   shymega = import inputs.nixpkgs-shymega {
     localSystem = final.stdenv.hostPlatform;
     config = inputs.self.nixpkgs-config;
-    overlays = builtins.attrValues shymegaOverlays;
+    # overlays = builtins.attrValues shymegaOverlays;
   };
 }
