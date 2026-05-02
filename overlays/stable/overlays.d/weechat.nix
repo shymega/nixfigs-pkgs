c1dbd62b1b6b20599227965273e369ae55adf2aa
Dom Rodriguez <shymega@shymega.org.uk>
2026-05-02 20:54:27 +0100

    feat: Use my Weechat fork
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- overlays/stable/overlays.d/weechat.nix
+++ overlays/stable/overlays.d/weechat.nix
@@ -3,19 +3,28 @@
 # SPDX-License-Identifier: GPL-3.0-only
 #
 _: prev: {
-  weechatWithMyPlugins = prev.weechat.override {
-    configure = {availablePlugins, ...}: {
-      scripts = with prev.weechatScripts; [
-        autosort
-        edit
-        url_hint
-        wee-slack
-        weechat-autosort
-        weechat-go
-        weechat-notify-send
-        zncplayback
-      ];
-      plugins = builtins.attrValues availablePlugins;
+  weechatWithMyPlugins =
+    (prev.weechat.overrideAttrs (oldAttrs: {
+      version = "4.10.0-dev";
+      src = prev.fetchFromGitHub {
+        owner = "shymega";
+        repo = "weechat";
+        rev = "cdcdfd7522d0aa723d05447068cbd36a49f6d064";
+        hash = "sha256-XNTuyQRH01NDW/lAQsf3gIrGDMhTP9NLL0r5gIO93Qw=";
+      };
+    })).override {
+      configure = {availablePlugins, ...}: {
+        scripts = with prev.weechatScripts; [
+          autosort
+          edit
+          url_hint
+          wee-slack
+          weechat-autosort
+          weechat-go
+          weechat-notify-send
+          zncplayback
+        ];
+        plugins = builtins.attrValues availablePlugins;
+      };
     };
-  };
 }
