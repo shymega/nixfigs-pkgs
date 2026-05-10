1b4aa7365ffdf83d258f0995a80f4ea3552e005f
Dom Rodriguez <shymega@shymega.org.uk>
2026-05-10 16:47:45 +0100

    fix: Fix weechat overlay
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- overlays/stable/overlays.d/weechat.nix
+++ overlays/stable/overlays.d/weechat.nix
@@ -2,29 +2,27 @@
 #
 # SPDX-License-Identifier: GPL-3.0-only
 #
-_: prev: {
-  weechatWithMyPlugins =
-    (prev.weechat.overrideAttrs (_oldAttrs: {
-      version = "4.10.0-dev";
-      src = prev.fetchFromGitHub {
-        owner = "shymega";
-        repo = "weechat";
-        rev = "cdcdfd7522d0aa723d05447068cbd36a49f6d064";
-        hash = "sha256-XNTuyQRH01NDW/lAQsf3gIrGDMhTP9NLL0r5gIO93Qw=";
-      };
-    })).override {
-      configure = {availablePlugins, ...}: {
-        scripts = with prev.weechatScripts; [
-          autosort
-          edit
-          url_hint
-          wee-slack
-          weechat-autosort
-          weechat-go
-          weechat-notify-send
-          zncplayback
-        ];
-        plugins = builtins.attrValues availablePlugins;
-      };
+final: prev: {
+  weechat = prev.weechat.overrideAttrs (_: {
+    version = "4.10.0-dev";
+    src = prev.fetchFromGitHub {
+      owner = "shymega";
+      repo = "weechat";
+      rev = "cdcdfd7522d0aa723d05447068cbd36a49f6d064";
+      hash = "sha256-XNTuyQRH01NDW/lAQsf3gIrGDMhTP9NLL0r5gIO93Qw=";
     };
+  });
+
+  weechatWithPlugins = final.weechat.override {
+    configure = {availablePlugins, ...}: {
+      scripts = with prev.weechatScripts; [
+        autosort
+        url_hint
+        weechat-autosort
+        weechat-go
+        weechat-notify-send
+      ];
+      plugins = builtins.attrValues availablePlugins;
+    };
+  };
 }
