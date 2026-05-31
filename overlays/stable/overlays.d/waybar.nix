1b867c8ae8d92bfb9cbe5e8ae3f2b62d6a9810c9
Dom Rodriguez <shymega@shymega.org.uk>
2026-05-31 16:59:14 +0100

    chore: Update Flake inputs
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- overlays/stable/overlays.d/waybar.nix
+++ overlays/stable/overlays.d/waybar.nix
@@ -3,11 +3,11 @@
 # SPDX-License-Identifier: GPL-3.0-only
 #
 _final: prev: {
-  waybar = prev.waybar.overrideAttrs (finalAttrs: {
+  waybar = prev.waybar.overrideAttrs (_finalAttrs: {
     patches = [
       (prev.fetchpatch {
-      	url = "https://patch-diff.githubusercontent.com/raw/Alexays/Waybar/pull/4982.patch";
-	hash = "sha256-acX+MOXN1aSGVA/sK+XEJ/FRAILS1f1JUXm1NHRl45I=";
+        url = "https://patch-diff.githubusercontent.com/raw/Alexays/Waybar/pull/4982.patch";
+        hash = "sha256-acX+MOXN1aSGVA/sK+XEJ/FRAILS1f1JUXm1NHRl45I=";
       })
     ];
   });
